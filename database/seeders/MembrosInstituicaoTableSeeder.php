51c47ba00a762e9195db14ae0aed1d9724c814d9
gustavorizzon <gustavop.rizzon@gmail.com>
2020-10-19 18:14:45 -0300

    feat: Adicionado usuario admin.

--- database/seeders/MembrosInstituicaoTableSeeder.php
+++ database/seeders/MembrosInstituicaoTableSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cidade;
 use App\Models\MembroInstituicao;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class MembrosInstituicaoTableSeeder extends Seeder
@@ -14,6 +16,30 @@ class MembrosInstituicaoTableSeeder extends Seeder
    */
   public function run()
   {
+    // Create Admin user
+    MembroInstituicao::create([
+      'nome' => 'Admin',
+      'data_nascimento' => Carbon::now()->toDateTimeString(),
+      'orientacao_sexual' => 'O',
+      'estado_civil' => 'S',
+      'cpf' => '000.000.000-00',
+      'rg' => '0000000000',
+      'nome_mae' => 'Mother Admin',
+      'nome_pai' => 'Father Admin',
+      'telefone' => '(54) 12345-6789',
+      'email' => 'admin@example.com',
+      'rua' => 'System Home',
+      'numero_rua' => 0,
+      'complemento' => 'Inside',
+      'referencia' => 'System',
+      'bairro' => 'Users',
+      'cidade_id' => Cidade::all()->random()->id,
+      'login' => 'admin',
+      'senha' => bcrypt('admin'),
+      'tipo_membro' => '*'
+    ]);
+
+    // Create 100 Members using factory
     MembroInstituicao::factory(100)->create();
   }
 }
