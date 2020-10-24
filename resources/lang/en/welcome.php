5b77bb9da1848fbb9b373feec501991101ca4f4b
gustavorizzon <gustavop.rizzon@gmail.com>
2020-10-24 12:53:36 -0300

    feat: Design da página inicial.

---
+++ resources/lang/en/welcome.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'page' => [
+    'title' => 'Academic Manager',
+    'description' => 'An application for Students, Professor and Coordinators.',
+    'header' => [
+      'title' => 'Manage your school institution with the Academic Manager',
+      'text' => 'Welcome to the Academic Manager, a tool built to help you and facilitate the management of your institution.'
+    ]
+  ]
+];
---
+++ resources/lang/pt-br/welcome.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'page' => [
+    'title' => 'Gerenciador Acadêmico',
+    'description' => 'Uma aplicação para Acadêmicos, Professores e Coordenadores.',
+    'header' => [
+      'title' => 'Gerencie sua instituição escolar com o Gerenciador Acadêmico',
+      'text' => 'Seja bem-vindo ao Gerenciador Acadêmico, uma ferramenta construída para ajudar e facilitar o gerenciamento da sua instituição.'
+    ]
+  ]
+];
