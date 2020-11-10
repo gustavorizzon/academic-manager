6faeb8d532f960162b3546776349177a6ddf913a
gustavorizzon <gustavop.rizzon@gmail.com>
2020-11-09 21:54:41 -0300

    lib: scripts.tempusdominus-bootstrap4

---
+++ resources/views/scripts/tempusdominus.blade.php
@@ -0,0 +1,11 @@
+@section('css')
+  <link rel="stylesheet" href="{{ asset('vendor/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+
+  @parent
+@endsection
+
+@section('js')
+	<script src="{{ asset('vendor/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+
+  @parent
+@endsection
